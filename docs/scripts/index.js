2f4b66c2f657a6679e58b52a57cb292d4dd1292f
Chris Blanquera <chris@incept.asia>
2024-03-11 13:58:34 +0800

    added more js effects

--- docs/scripts/index.js
+++ docs/scripts/index.js
@@ -1,4 +1,31 @@
 (() => {
+  const modal = document.getElementById('modal');
+  const frame = document.getElementById('google-frame');
+  const success = document.getElementById('success');
+  let ready = false;
+  
+  frame.addEventListener('load', () => {
+    if (!ready) {
+      ready = true;
+      return;
+    }
+    modal.style.display = 'none';
+    success.style.display = 'block';
+    setTimeout(() => {
+      success.style.display = 'none';
+    }, 5000)
+  });
+  window.addEventListener('modal-open-click', e => {
+    const label = e.for.getAttribute('data-label');
+    const type = e.for.getAttribute('data-type');
+    modal.querySelector('h2').innerText = label;
+    modal.querySelector('form button').innerText = label;
+    document.getElementById('type').value = type;
+    modal.style.display = 'flex';
+  });
+  window.addEventListener('modal-close-click', e => {
+    modal.style.display = 'none';
+  });
   window.addEventListener('tab-click', e => {
     e.for.parentNode.querySelectorAll('a').forEach((tab) => {
       tab.classList.remove('active');
